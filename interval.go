f9339025b3dc5cfccceb47dfdc704eced69e6fd4
Meiko Schimmelpfennig <schimmelpfennig@awesomeprototype.com>
2023-09-02 09:09:29 +0200

    fist simple tested implementation of ferge function

--- interval.go
+++ interval.go
@@ -20,7 +20,7 @@ type Limit[T Numeric] struct {
 }
 
 func Merge[T Numeric](intervals ...Interval[T]) ([]Interval[T], error) {
-	// concideration: this will modify this initial slice: in order to avoid this a copy could be created which would cause additional allocation
+	// consideration: this will modify this initial slice: in order to avoid this a copy could be created which would cause additional allocation
 	sort.SliceStable(intervals, func(a, b int) bool {
 		intervalA := intervals[a]
 		intervalB := intervals[b]
@@ -33,7 +33,26 @@ func Merge[T Numeric](intervals ...Interval[T]) ([]Interval[T], error) {
 		return intervalA.Min.Value < intervalB.Min.Value
 	})
 
-	return intervals, nil
+	var result []Interval[T]
+	var current *Interval[T]
+	for i, interval := range intervals {
+		if current == nil {
+			// important to use intervals[i] instead of interval since in don't want a pointer to the interval variable that will change in every iteration
+			current = &intervals[i]
+			continue
+		}
+		if merged, didMerge := current.merge(interval); didMerge {
+			current = &merged
+		} else {
+			result = append(result, *current)
+			current = &intervals[i]
+		}
+	}
+	if current != nil {
+		result = append(result, *current)
+	}
+
+	return result, nil
 }
 
 func (interval Interval[T]) String() string {
@@ -50,5 +69,33 @@ func (interval Interval[T]) String() string {
 
 func (interval Interval[T]) merge(other Interval[T]) (Interval[T], bool) {
 
-	return Interval[T]{}, false
+	if interval.Max.Value < other.Min.Value {
+		return Interval[T]{}, false
+	} else if interval.Max.Value == other.Min.Value {
+		if interval.Max.Open || other.Min.Open {
+			return Interval[T]{}, false
+		}
+	}
+
+	//yes we do have an intersection
+	if interval.Max.Value > other.Min.Value { // interval has the greater upper limit
+		return Interval[T]{
+			Min: interval.Min,
+			Max: interval.Max,
+		}, true
+	} else if interval.Max.Value == other.Max.Value { // limits are semi equal
+		return Interval[T]{
+				Min: interval.Min,
+				Max: Limit[T]{
+					Value: other.Max.Value,
+					Open:  interval.Max.Open && other.Max.Open,
+				},
+			},
+			true
+	}
+	// other has the upper limit
+	return Interval[T]{
+		Min: interval.Min,
+		Max: other.Max,
+	}, true
 }
